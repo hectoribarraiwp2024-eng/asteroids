c67118f5ed2b85e92409ab394b7aafd6a7cd234f
hectoribarraiwp2024-eng <hector.ibarra.iwp2024@gmail.com>
2026-02-17 15:46:41 -0600

    changed the fps to 60 in main.py

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -20,7 +22,10 @@ def main():
 
         screen.fill("black")
 
+        dt = clock.tick(60) / 1000
+        
         pygame.display.flip()
+    
 
 
 if __name__ == "__main__":
