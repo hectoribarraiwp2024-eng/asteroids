2aad13e5ab8c05654e242c0a4e148454aa3fb556
hectoribarraiwp2024-eng <hector.ibarra.iwp2024@gmail.com>
2026-02-18 17:36:55 -0600

    Done! now the asteroids split if hit and the game is complete

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 from circleshape import CircleShape
 import pygame
-from constants import LINE_WIDTH    
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from logger import  log_event
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -12,3 +14,22 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += (self.velocity * dt)
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        log_event("asteroid_split")
+
+        angle = random.uniform(20, 50)
+
+        vel1 = self.velocity.rotate(angle)
+        vel2 = self.velocity.rotate(-angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroid1 = Asteroid(self.position[0], self.position[1], new_radius)
+        asteroid2 = Asteroid(self.position[0], self.position[1], new_radius)
+
+        asteroid1.velocity = vel1 * 1.2
+        asteroid2.velocity = vel2 * 1.2
