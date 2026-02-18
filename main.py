ae2e4de7efae9774ac4a59d4f2bee98da9d12bcd
hectoribarraiwp2024-eng <hector.ibarra.iwp2024@gmail.com>
2026-02-18 16:22:31 -0600

    now we are using groups (updatable and drawable) update and draw instead of the player objects

--- main.py
+++ main.py
@@ -13,8 +13,13 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    
     while True:
         log_state()
 
@@ -26,8 +31,10 @@ def main():
 
         dt = clock.tick(60) / 1000
 
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        
+        for thing in drawable:
+            thing.draw(screen)
         
         pygame.display.flip()
     
