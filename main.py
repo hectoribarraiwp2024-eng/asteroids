38cacaad28f4fea165ad3b4df3b6058d3979dc8b
hectoribarraiwp2024-eng <hector.ibarra.iwp2024@gmail.com>
2026-02-18 15:59:40 -0600

    create a player.py file and a draw method so now the player is drawn to the middle of the screen during the game loop

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(f'Starting Asteroids with pygame version: {pygame.version.ver}')
@@ -12,6 +13,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -23,6 +25,8 @@ def main():
         screen.fill("black")
 
         dt = clock.tick(60) / 1000
+
+        player.draw(screen)
         
         pygame.display.flip()
     
