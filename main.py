2291389deff1eff97cd1e1f79530dbc7fbd6ebcc
hectoribarraiwp2024-eng <hector.ibarra.iwp2024@gmail.com>
2026-02-18 16:38:58 -0600

    added asteroids by creating asteroid.py and their functionality by creating asteroidfield.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print(f'Starting Asteroids with pygame version: {pygame.version.ver}')
@@ -16,9 +18,15 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
 
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
+
     
     while True:
         log_state()
@@ -32,7 +40,7 @@ def main():
         dt = clock.tick(60) / 1000
 
         updatable.update(dt)
-        
+
         for thing in drawable:
             thing.draw(screen)
         
