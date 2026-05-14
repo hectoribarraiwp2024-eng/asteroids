a7482351c6550579d5aa7683d88f6ea0c2ec9790
hectoribarraiwp2024-eng <hector.ibarra.iwp2024@gmail.com>
2026-05-14 15:04:40 -0500

    updated the fps logic

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -15,6 +18,9 @@ def main():
 
         screen.fill("black")
 
+        delta = clock.tick(60)
+        dt = delta / 1000
+        print(dt)
         pygame.display.flip()
 
 
