a84d6610305d45368a2563b8b03d9ea87be0d42a
hectoribarraiwp2024-eng <hector.ibarra.iwp2024@gmail.com>
2026-05-14 15:28:50 -0500

    updated constants, player methods rotate and update, and the game loop player logic

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
-
+from player import Player
 
 def main():
     pygame.init()
@@ -10,18 +10,26 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill("black")
+        player.update(dt)
 
-        delta = clock.tick(60)
-        dt = delta / 1000
-        print(dt)
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        #limits the fps to 60
+        dt = clock.tick(60) / 1000
+
+        
 
 
 if __name__ == "__main__":
