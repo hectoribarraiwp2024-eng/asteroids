76445540245c207ded89c55a993a50e10fc2c8fe
hectoribarraiwp2024-eng <hector.ibarra.iwp2024@gmail.com>
2026-05-14 16:19:01 -0500

    updated collision logic

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+import sys
 
 def main():
     pygame.init()
@@ -33,6 +34,12 @@ def main():
                 return
 
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit(1)
 
         screen.fill("black")
         
