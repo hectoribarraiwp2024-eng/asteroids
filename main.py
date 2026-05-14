2a20769f9bdf8e466ee5501e7139bc1a3d1c9b49
hectoribarraiwp2024-eng <hector.ibarra.iwp2024@gmail.com>
2026-05-14 16:12:05 -0500

    updated the asteroid logic

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -15,10 +17,14 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.countainers = updatable
     
     player = Player(x, y)
+    asteroid_field = AsteroidField()
 
     while True:
         log_state()
