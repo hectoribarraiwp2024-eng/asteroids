a41b02ad7f008267e1d69213ecdde25ddeea251b
hectoribarraiwp2024-eng <hector.ibarra.iwp2024@gmail.com>
2026-09-02 13:33:17 -0500

    added containers

--- main.py
+++ main.py
@@ -13,10 +13,17 @@ def main():
     clock = pygame.time.Clock()
     dt = 0.0
 
+    #instantiate groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     #instantiate player
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
+
 
     #GAME LOOP
     while True:
@@ -26,10 +33,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
     
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
